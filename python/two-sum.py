21bf368fdfb6e76fca58879977b0b4780b57d24a
Yish <mombuartworks@gmail.com>
2020-03-24 23:49:16 +0800

    Update two-sum.py

--- python/two-sum.py
+++ python/two-sum.py
@@ -11,3 +11,17 @@ class Solution:
             hash[num] = index
 
 print((Solution()).twoSum([2,7,11,15],9))
+
+
+## Faster
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        dict = {}
+        
+        for k in range(len(nums)):
+          r = target-nums[k]
+
+          if dict.get(r) is not None:
+            return [dict.get(r), k]
+
+          dict[nums[k]] = k
